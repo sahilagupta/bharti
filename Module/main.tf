d08c93a6d40df8b346f3bd00bd2148873c960772
Sahil Gupta <sahil.a.gupta145@gmail.com>
2025-07-06 15:21:53 +0530

    new rg2 add

--- Module/main.tf
+++ Module/main.tf
@@ -4,6 +4,13 @@ module "rg" {
   location = "australia east"
 }
 
+module "rg2" {
+  source   = "../child/RG"
+  name     = "rohit-rg2"
+  location = "australia east"
+}
+
+
 module "vnet" {
   depends_on          = [module.rg]
   source              = "../child/VNET"
